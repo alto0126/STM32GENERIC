295f3fc4836278eec8793780ffd1ccd6e23bf1bc
alto0126 <31755350+alto0126@users.noreply.github.com>
2019-05-08 22:55:19 +0900

    Add files via upload

---
+++ STM32/variants/ARM-First_F405/variant.h
@@ -0,0 +1,26 @@
+#ifndef VARIANT_H
+#define VARIANT_H
+
+//On-board LED pin number           PIN  // Arduino Pin Number         
+#define LED_BUILTIN                 PB4 // 18
+#define LED_GREEN                   LED_BUILTIN
+#define LED_BLUE                    LED_BUILTIN
+#define LED_RED                     PA15 // 19
+#define LED_ORANGE                  LED_RED
+#define LED_RED_OTG_OVERCURRENT     LED_RED // be careful with this led. When using it configure the output as PULL_UP.
+
+//On-board user button
+#define USER_BTN                    PC13  // 2
+
+
+// Connected to on board LIS3DSH accelerometer, CS: PE3
+#define MOSI                        PA7
+#define MISO                        PA6
+#define SCK                         PA5
+#define SS                          PA4
+
+// Connected to on board CS43L22 audio
+#define SDA                         PB7
+#define SCL                         PB6
+
+#endif
